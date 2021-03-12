cc1aa1c0ebaf78f5246052277ab82f5f4e017888
Ben Feist <bf@benfeist.com>
2021-03-12 17:10:15 -0500

    still setting up

--- webpack.common.js
+++ webpack.common.js
@@ -1,138 +1,129 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
-const dirNode = 'node_modules';
-const dirApp = path.join(__dirname, 'app');
-const dirStyles = path.join(__dirname, 'styles');
-const dirAssets = path.join(__dirname, 'assets');
+const dirNode = "node_modules";
+const dirApp = path.join(__dirname, "app");
+const dirStyles = path.join(__dirname, "styles");
+const dirAssets = path.join(__dirname, "assets");
 
 /**
  * Webpack Configuration
  */
-module.exports = env => {
+module.exports = (env) => {
     // Is the current build a development build
     const IS_DEV = !!env.dev;
 
     return {
-
         entry: {
-            main: path.join(dirApp, 'index')
+            main: path.join(dirApp, "index"),
         },
 
         resolve: {
-            modules: [
-                dirNode,
-                dirApp,
-                dirStyles,
-                dirAssets
-            ]
+            modules: [dirNode, dirApp, dirStyles, dirAssets],
         },
 
         plugins: [
             new webpack.DefinePlugin({ IS_DEV }),
 
             new HtmlWebpackPlugin({
-                template: path.join(__dirname, 'index.ejs'),
-                title: 'Webpack Boilerplate'
+                template: path.join(__dirname, "index.ejs"),
+                title: "ISS Position",
             }),
 
             new ImageMinimizerPlugin({
                 minimizerOptions: {
                     plugins: [
                         [
-                            'imagemin-svgo',
+                            "imagemin-svgo",
                             {
                                 plugins: [
                                     // SVGO options: "https://github.com/svg/svgo#what-it-can-do"
                                     {
                                         removeViewBox: false,
-                                        removeXMLNS: true
-                                    }
-                                ]
-                            }
-                        ]
-                    ]
-                }
-            })
+                                        removeXMLNS: true,
+                                    },
+                                ],
+                            },
+                        ],
+                    ],
+                },
+            }),
         ],
 
         module: {
             rules: [
-                // BABEL    
+                // BABEL
                 {
                     test: /\.m?js$/,
                     exclude: /(node_modules)/,
                     use: {
-                        loader: 'babel-loader',
+                        loader: "babel-loader",
                         options: {
-                            compact: true
-                        }
-                    }
+                            compact: true,
+                        },
+                    },
                 },
 
                 // STYLES
                 {
                     test: /\.css$/,
                     use: [
-                        'style-loader',
+                        "style-loader",
                         {
-                            loader: 'css-loader',
+                            loader: "css-loader",
                             options: {
-                                sourceMap: IS_DEV
-                            }
+                                sourceMap: IS_DEV,
+                            },
                         },
-                    ]
+                    ],
                 },
 
                 // CSS / SASS
                 {
                     test: /\.scss/,
                     use: [
-                        'style-loader',
+                        "style-loader",
                         {
-                            loader: 'css-loader',
+                            loader: "css-loader",
                             options: {
-                                sourceMap: IS_DEV
-                            }
+                                sourceMap: IS_DEV,
+                            },
                         },
                         {
-                            loader: 'sass-loader',
+                            loader: "sass-loader",
                             options: {
                                 sourceMap: IS_DEV,
                                 sassOptions: {
-                                    includePaths: [dirAssets]
-                                }
-                            }
-                        }
-                    ]
+                                    includePaths: [dirAssets],
+                                },
+                            },
+                        },
+                    ],
                 },
 
                 // IMAGES
                 {
                     test: /\.(png|jpe?g|gif)$/i,
                     use: {
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
-                            name: '[path][name].[ext]'
-                        }
-                    }
+                            name: "[path][name].[ext]",
+                        },
+                    },
                 },
 
                 // SVG
                 {
                     test: /\.svg$/,
-                    use: [
-                        'raw-loader'
-                    ]
-                }
-            ]
+                    use: ["raw-loader"],
+                },
+            ],
         },
 
         optimization: {
-            runtimeChunk: 'single'
-        }
-
+            runtimeChunk: "single",
+        },
     };
 };
